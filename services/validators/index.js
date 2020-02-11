a9f526c16fbecb468752bf5845a890722633051b
Caio Majdalani <caio.melo.ext@sodexo.com>
2020-02-11 16:18:56 -0300

    add some validators

--- services/validators/index.js
+++ services/validators/index.js
@@ -3,22 +3,22 @@ const Joi = require('@hapi/joi');
 const schema = {
     project: Joi.object({
         body: Joi.object({
-            name: Joi.string().valid('PedeFacil', 'Portal', 'Ecommerce', 'Allowance', 'SodexoMobile', 'PartnersBridge', 'BUS', 'Gateway', 'Test'),
-            platform: Joi.string().valid('API', 'WEB', 'Mobile', 'Test')
+            name: Joi.string().valid('PedeFacil', 'Portal', 'Ecommerce', 'Allowance', 'SodexoMobile', 'PartnersBridge', 'BUS', 'Gateway', 'Test').required(),
+            platform: Joi.string().valid('API', 'WEB', 'Mobile', 'Test').required()
         })
     }).unknown(),
     report: Joi.object({
         body: Joi.object({
-            projectId: Joi.number().integer(),
-            executionDate: Joi.date().iso(),
+            projectId: Joi.number().integer().min(1).required(),
+            executionDate: Joi.date().iso().required(),
             report: Joi.object({
                 features: Joi.object({
-                    total: Joi.number().integer(),
-                    failures: Joi.number().integer()
+                    total: Joi.number().integer().min(1).required(),
+                    failures: Joi.number().integer().max(Joi.ref('total')).min(0)
                 }),
                 scenarios: Joi.object({
-                    total: Joi.number().integer(),
-                    failures: Joi.number().integer()
+                    total: Joi.number().integer().min(1).required(),
+                    failures: Joi.number().integer().max(Joi.ref('total')).min(0)
                 })
             })
         })
